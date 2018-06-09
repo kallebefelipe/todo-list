e8c9f29635cf2d037f163c1df1c75a19e1d31c93
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-09 16:12:30 -0300

    Atualizando urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,23 +1,17 @@
-"""todo_list URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
+from todos import views
+
+api_routers = routers.SimpleRouter()
+api_routers.register(r'todos', views.TodoViewSet)
+api_routers.register(r'tasks', views.TaskViewSet)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^docs/', include_docs_urls(title='Todo List')),
+    path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title='Todo List')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include((api_routers.urls, 'todos', )))
 ]
