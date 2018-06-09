332554efe8b3d952c96ec23644d39de3c3202147
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-09 13:10:11 -0300

    Adicionando urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^docs/', include_docs_urls(title='Todo List')),
 ]
