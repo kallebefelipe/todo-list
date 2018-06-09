a4311ac7b852fa9ba1441da57b36ea9104b91c27
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-09 11:52:21 -0300

    Atualizando models para os campos serem required

--- todos/models.py
+++ todos/models.py
@@ -3,21 +3,24 @@ from django.db import models
 
 
 class Todo(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=False, default=None)
     user = models.ForeignKey(
-        User, related_name='todos', on_delete=models.CASCADE)
+        User, related_name='todos', on_delete=models.CASCADE, blank=False,
+        null=False)
 
     def __str__(self):
         return self.name
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=False, default=None)
     deadline = models.DateTimeField()
     user = models.ForeignKey(
-        User, related_name='tasks', on_delete=models.CASCADE)
+        User, related_name='tasks', on_delete=models.CASCADE, blank=False,
+        null=False)
     todo = models.ForeignKey(
-        'Todo', related_name='tasks', on_delete=models.CASCADE)
+        'Todo', related_name='tasks', on_delete=models.CASCADE, blank=False,
+        null=False)
 
     def __str__(self):
         return self.name
