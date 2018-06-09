2b95c77cef2816d22d1b37a9a6e59d5c28131776
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-09 16:16:54 -0300

    Atualizando views

--- todos/views.py
+++ todos/views.py
@@ -1,19 +1,23 @@
 from rest_framework import viewsets
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from . import models
 from . import serializers
 
 
-class TodoViewSet(viewsets.ModelViewSet):
+class TodoViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
     queryset = models.Todo.objects.all()
     serializer_class = serializers.TodoSerializer
 
+    def get_queryset(self):
+        if not self.request.user.is_superuser:
+            return models.Todo.objects.filter(user=self.request.user)
+        return self.queryset.all()
 
-class TaskViewSet(viewsets.ModelViewSet):
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TaskViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
     queryset = models.Task.objects.all()
     serializer_class = serializers.TaskSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = models.User.objects.all()
-    serializer_class = serializers.UserSerializer
