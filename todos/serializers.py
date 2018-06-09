d2fdbf2fa1c27436fd13d6c797fc8c34b841ebf7
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-09 16:15:38 -0300

    Atualizando serializers

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from . import models
 
@@ -5,19 +6,18 @@ from . import models
 class TodoSerializer(serializers.ModelSerializer):
     name = serializers.CharField(
         max_length=255, allow_blank=False, required=True)
+    user = serializers.ReadOnlyField(source='user.id')
 
     class Meta:
         model = models.Todo
         fields = ['id', 'name', 'user']
-
-    def __str__(self):
-        return self.name
 
 
 class TaskSerializer(serializers.ModelSerializer):
     name = serializers.CharField(
         max_length=100, allow_blank=False, required=True
     )
+    user = serializers.ReadOnlyField(source='user.id')
 
     class Meta:
         model = models.Task
@@ -25,4 +25,17 @@ class TaskSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    pass
+    password = serializers.CharField(
+        max_length=100, required=True, allow_blank=False,
+        write_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+
+    def create(self, validated_data):
+        user = User(username=validated_data.get('username', None))
+        user.set_password(validated_data.get('password', None))
+        user.save()
+        return user
