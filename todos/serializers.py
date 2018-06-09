d36985788303b91f85ffdab9d15796ec6373a6e7
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-09 13:11:08 -0300

    Adicionando serializers de tasks e to-do

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,15 +1,27 @@
 from rest_framework import serializers
+from . import models
 
 
 class TodoSerializer(serializers.ModelSerializer):
-    name = models.CharField(max_length=255)
+    name = serializers.CharField(
+        max_length=255, allow_blank=False, required=True)
+
+    class Meta:
+        model = models.Todo
+        fields = ['id', 'name', 'user']
 
     def __str__(self):
         return self.name
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    pass
+    name = serializers.CharField(
+        max_length=100, allow_blank=False, required=True
+    )
+
+    class Meta:
+        model = models.Task
+        fields = ['id', 'name', 'todo', 'user']
 
 
 class UserSerializer(serializers.ModelSerializer):
