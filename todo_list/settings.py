047b1f5646795a91a1d3f5c5d9b5b163ec8792a1
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-11 07:22:29 -0300

    Adicionando app de frontend no settings

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'todos',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'), )
