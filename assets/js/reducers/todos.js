56a5b56dbe3e0c184a15fe943ca3bff8c32c49ab
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-11 07:28:15 -0300

    Adicionando reducer todo

---
+++ assets/js/reducers/todos.js
@@ -0,0 +1,17 @@
+const todosState = {
+    todos: [
+        {
+            id: 3
+            name: 'nome',
+            tasks: [
+                {
+                    id: 1
+                    name: 'task',
+                    deadline: "2018-06-10T18:16:58Z",
+                    todo: 1,
+                    user: 'kallebe'
+                }
+            ]
+        }
+    ]
+}
