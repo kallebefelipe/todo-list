809ed3c32ac58e7b2579643c22866143ee631557
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-12 01:03:08 -0300

    Atualizando componente AddTodoForm

--- assets/js/components/AddTodoForm.js
+++ assets/js/components/AddTodoForm.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addTodo } from '../actions/todos';
 
 
-const AddTodoForm = () => (
-    <form>
-        <input type="text" placeholder="Name" />
-        <button type="submit">Add todo list</button>
-    </form>
-);
+class AddTodoForm extends React.Component {
+    state = {
+        inputValue: ''
+    }
 
-export default AddTodoForm;
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this)
+        this.props.dispatch(addTodo({
+            name: this.state.inputValue,
+            tasks: []
+        }));
+    };
+
+    render() {
+        return (
+            <form>
+                <input
+                    onChange={(e) => {
+                        const value = e.target.value;
+                        this.setState(() => ({
+                            inputValue: value
+                        }));
+                    }} type="text" placeholder="Name" />
+                <button type="submit" onClick={this.handleSubmit}>Add todo list</button>
+            </form>
+        );
+    }
+
+};
+
+export default connect()(AddTodoForm);
