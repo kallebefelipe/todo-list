212eadf6aea0b407bca1f5b20a288a8f8ba3b7cd
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-12 01:04:13 -0300

    Atualizando testes da view

--- todos/tests/test_views.py
+++ todos/tests/test_views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 from model_mommy import mommy
 from todos import models
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 import json
 
 
@@ -142,3 +145,119 @@ class TaskViewSetTests(TestCase):
         self.client.login(username=self.user.username, password='123')
         response = self.client.delete('/api/tasks/1/')
         self.assertEqual(response.status_code, 204)
+
+
+class AccountsTest(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(
+            'testuser', 'test@example.com', 'testpassword'
+        )
+        self.create_url = reverse('account-create')
+
+    def test_create_user(self):
+        data = {
+            'username': 'foobar',
+            'email': 'foobar@example.com',
+            'password': 'somepassword'
+        }
+
+        response = self.client.post(
+            self.client.post(self.create_url), data, format='json'
+        )
+
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(response.statur_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['email'], data['email'])
+        self.assertFalse('password' in response.data)
+
+    def test_create_user_with_short_password(self):
+        data = {
+            'username': 'foobar',
+            'email': 'foobarbaz@example.com',
+            'password': 'foo'
+        }
+
+        response = self.client.post(
+            self.create_url, data, format='json'
+        )
+
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST
+        )
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['password']), 1)
+
+    def test_create_user_with_no_password(self):
+        data = {
+            'username': 'foobar',
+            'email': 'foobarbaz@example.com',
+            'password': ''
+        }
+
+        response = self.client.post(
+            self.create_url, data, format='json'
+        )
+        self.assertEqual(
+            response.status_code, self.HTTP_400_BAD_REQUEST
+        )
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['password']), 1)
+
+    def test_create_user_with_too_long_username(self):
+        data = {
+            'username': 'foo'*30,
+            'email': 'foobarbaz@example.com',
+            'password': 'foobas'
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['username']), 1)
+
+        def test_create_user_with_preexisting_username(self):
+            data = {
+                'username': 'testuser',
+                'email': 'user@example.com',
+                'password': 'testuser'
+            }
+
+            response = self.client.post(self.create_url, data, format='json')
+            self.assertEqual(User.objects.count(), 1)
+            self.assertEqual(len(response.data['username']), 1)
+
+        def test_create_user_with_preexisting_email(self):
+            data = {
+                'username': 'testuser2',
+                'email': 'test@example.com',
+                'password': 'testuser'
+            }
+
+            response = self.client.post(self.create_url, data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            self.assertEqual(User.objects.count(), 1)
+            self.assertEqual(len(response.data['email']), 1)
+
+        def test_create_user_with_no_email(self):
+            data = {
+                    'username': 'foobar',
+                    'email': '',
+                    'password': 'foobarbaz'
+            }
+
+            response = self.client.post(self.create_url, data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            self.assertEqual(User.objects.count(), 1)
+            self.assertEqual(len(response.data['email']), 1)
+
+        def test_create_user(self):
+            data = {
+                'username': 'foobar',
+                'email': 'foobar@example.com',
+                'password': 'somepassword'
+            }
+
+            response = self.client.post(self.create_url, data, format='json')
+            user = User.objects.latest('id')
+            token = Token.objects.get(user=user)
+            self.assertEqual(response.data['token'], token.key)
