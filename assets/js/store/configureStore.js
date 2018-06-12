52bfcfa45673a83f12dd6bbafb9481a73674140e
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-12 01:09:22 -0300

    Atualizando configureStore

--- assets/js/store/configureStore.js
+++ assets/js/store/configureStore.js
@@ -1,9 +1,14 @@
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
 import todoReducer from '../reducers/todos';
 
 
 const setUpStore = () => {
-    const store = createStore(todoReducer);
+    const store = createStore(
+        combineReducers({
+            todoReducer
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
     return store;
 };
 
