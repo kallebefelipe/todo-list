3e4410476cc290765d14595c3c3401c4110791be
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-13 17:08:24 -0300

    Atualizando api de to-do

--- assets/js/api/TodosApi.js
+++ assets/js/api/TodosApi.js
@@ -14,13 +14,42 @@ class TodosApi {
     }
 
     static deleteTodo(data) {
-        return fetch('/api/todos/'+data.todo.id,{
+        return fetch('/api/todos/'+data.id,{
                 method: 'DELETE',
                 headers: {
                         'Authorization': "Token a454e53304779130a63789f2440f505182679f8d",
                         'Content-Type': 'application/x-www-form-urlencoded',
                     },
                 }
+
+            ).then((response) =>  {
+                return response.status;
+            })
+    }
+
+    static addTodo(data) {
+        return fetch('/api/todos/', {
+                method: 'POST',
+                headers: {
+                    'Authorization': "Token a454e53304779130a63789f2440f505182679f8d",
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            }).then((response) =>  {
+                return response.json();
+            })
+    }
+
+    static updateTodo(data) {
+        return fetch(`api/todos/${data.id}/`,{
+                method: 'PUT',
+                headers: {
+                        'Authorization': "Token a454e53304779130a63789f2440f505182679f8d",
+                        'Content-type': 'application/json',
+                    },
+                body: JSON.stringify(data)
+                },
 
             ).then((response) =>  {
                 return response.status;
