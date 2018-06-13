96c1e4756c97177cf9bf03b81a39763592defb28
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-13 09:43:27 -0300

    Atualizando componente app

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import TodoApp from './components/TodoApp';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { Provider } from 'react-redux';
+import setUpStore from './store/configureStore';
+import { Route, Switch, BrowserRouter} from 'react-router-dom';
+import { loadTodos } from './actions/todos';
+const store = setUpStore();
 
+const Jsx = () => (
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' component={TodoApp} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+);
 
-ReactDOM.render(<TodoApp />, document.getElementById('app'));
+ReactDOM.render(<Jsx />, document.getElementById('app'));
