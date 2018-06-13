aa12189cf95c49b8558d3dd67d76d6b599a208a3
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-13 09:48:43 -0300

    Atualizando reducer de to-dos

--- assets/js/reducers/todos.js
+++ assets/js/reducers/todos.js
@@ -1,3 +1,5 @@
+import { browserHistory } from 'react-router';
+
 const todosState = {
     todos: []
 }
@@ -8,12 +10,23 @@ const todoReducer = (state= todosState, action) => {
             return {
                 todos: action.data
             };
-            break;
         case 'ADD_TODO':
             return {
                 todos: state.todos.concat(action.todo)
             };
-            break;
+        case 'LOAD_TODO':
+           return {
+                todos: Object.assign([], state, action.todos)
+            };
+        case 'DELETE_TODO_SUCCESS': {
+            const newState = Object.assign([], state);
+            var pos;
+            state.todos.filter(function(todos, index) { pos = index; return todos.id == action.todo.id})
+            newState.todos.splice(pos, 1);
+            return {
+                todos: newState.todos
+            };
+        }
         default:
             return state
     }
