c4f8562c474d681034a5c00cbf6fcba40c67bc09
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-13 09:46:58 -0300

    Atualizando configureStore

--- assets/js/store/configureStore.js
+++ assets/js/store/configureStore.js
@@ -1,12 +1,16 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware } from 'redux';
 import todoReducer from '../reducers/todos';
+import authReducer from '../reducers/auth';
+import thunk from 'redux-thunk';
 
 
 const setUpStore = () => {
     const store = createStore(
         combineReducers({
-            todoReducer
+            todoReducer,
+            authReducer
         }),
+        applyMiddleware(thunk),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
     return store;
