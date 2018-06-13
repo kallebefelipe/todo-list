27a8ef4fdc9c20a61dde41efc9b46c6a12790ab9
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-13 09:47:22 -0300

    Atualizando componente de to-do

--- assets/js/components/TodoApp.js
+++ assets/js/components/TodoApp.js
@@ -1,24 +1,48 @@
-import React from 'react';
+ import React from 'react';
+import PropTypes from 'prop-types';
 import Header from './Header';
-import AddTodoForm from './AddTodoForm';
-import TodoList from './TodoList';
+import AddTodoForm from './todos/AddTodoForm';
+import TodoList from './todos/TodoList';
+import { connect } from 'react-redux';
+import { loadTodos } from '../actions/todos';
+import { populateInitialState } from '../actions/todos';
 
 
 class TodoApp extends React.Component {
-    state = {
-        data: undefined
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            data: undefined,
+        };
+    }
+
+    componentDidMount() {
+        fetch('/api/todos/',{
+                method: 'GET',
+                headers: {
+                    'Authorization': "Token a454e53304779130a63789f2440f505182679f8d",
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            })
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.props.dispatch(populateInitialState(responseData));
+                this.setState(() => ({
+                    data:responseData
+                }));
+            })
     }
 
     render() {
-        console.log("vai renderizar")
+        const todos = this.state.data;
         return (
             <div>
                 <Header />
                 <AddTodoForm />
-                <TodoList />
+                <TodoList todos={todos} />
             </div>
         );
     }
 }
 
-export default TodoApp;
+export default connect()(TodoApp);
