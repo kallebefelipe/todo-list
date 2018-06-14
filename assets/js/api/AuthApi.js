56b9bbfdbe95ad158ce9f6003eb04acf4ec7122c
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-13 23:59:38 -0300

    Atualizando ap de autenticação com registro de usuário

--- assets/js/api/AuthApi.js
+++ assets/js/api/AuthApi.js
@@ -14,6 +14,20 @@ class AuthApi {
                 return response.json();
             })
     }
+
+    static registerUser(data) {
+        return fetch('api/auth/register/', {
+                method: 'POST',
+                headers: {
+                        'Content-type': 'application/json',
+                    },
+                body: JSON.stringify(data)
+                },
+
+            ).then((response) =>  {
+                return response.json();
+            })
+    }
 }
 
 export default AuthApi;
