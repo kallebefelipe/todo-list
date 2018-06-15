f1ec8872a20ca26c1bbd8a3efd642e962283e7c9
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-15 00:45:56 -0300

    Atualizando componente principal app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,15 @@
+import 'normalize.css/normalize.css';
+import AddTaskForm from './components/tasks/AddTaskForm';
+import Login from './components/auth/Login';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import TodoApp from './components/TodoApp';
-import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import { Provider } from 'react-redux';
 import setUpStore from './store/configureStore';
-import { Route, Switch, BrowserRouter} from 'react-router-dom';
+import TodoApp from './components/TodoApp';
 import { loadTodos } from './actions/todos';
+import { Provider } from 'react-redux';
+import { Route, Switch, BrowserRouter} from 'react-router-dom';
+
 const store = setUpStore();
 
 const Jsx = () => (
@@ -15,6 +17,7 @@ const Jsx = () => (
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={Login} />
+                <Route path='/add_task' component={AddTaskForm} />
                 <Route path='/app' component={TodoApp} />
                 <Route exact path='/register' component={Register} />
             </Switch>
