abf645b7b8de5da3efa638dc0b788a8fee233c87
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-15 00:41:50 -0300

    Inclusão de popup no form de adicionar task

--- assets/js/components/tasks/AddTaskForm.js
+++ assets/js/components/tasks/AddTaskForm.js
@@ -1,32 +1,65 @@
 import React from 'react';
 import { addTask } from '../../actions/tasks';
+import { Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
+import { Link, browserHistory} from 'react-router';
+import { updateTask } from '../../actions/tasks';
 
 
 class AddTaskForm extends React.Component {
     state = {
-        inputValue: ''
+        name: '',
+        deadline: '',
+        task: {
+            name: ''
+        }
+    }
+
+    componentWillMount() {
+        if (this.props.task === undefined){
+            this.setState(() => (
+                {name: ''}
+            ));
+        }
     }
 
     handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.addNewTask({
-            name: this.state.inputValue,
-            todo: this.props.todo,
+        e.preventDefault()
+        if (this.props.task) {
+            this.props.mapUpdateTask({
+                name: this.state.name,
+                task: this.props.task.id,
+                todo: this.props.todo.id,
         }, this.props.token);
+        }else{
+            this.props.addNewTask({
+                name: this.state.name,
+                todo: this.props.todo.id,
+                deadline: this.state.deadline
+            }, this.props.token);
+        }
+    };
+
+    updateValue = (e, data) => {
+        const value = e.target.value;
+        this.setState(() => (data));
     };
 
     render() {
         return (
             <form>
                 <input
-                    onChange={(e) => {
-                        const value = e.target.value;
-                        this.setState(() => ({
-                            inputValue: value
-                        }));
-                    }} type="text" placeholder="Name" />
-                <button type="submit" onClick={this.handleSubmit}>Add task</button>
+                    onChange={(e) => {this.updateValue(e,
+                    {name: e.target.value})}}
+                    type="text" placeholder="Name"
+                    value={this.state.name}
+                    />
+                <input
+                    onChange={(e) => {this.updateValue(e,
+                    {deadline: e.target.value})}}
+                    type="date"
+                    />
+                <Button type="submit" onClick={this.handleSubmit}>Save</Button>
             </form>
         );
     }
@@ -43,6 +76,9 @@ const mapDispatchToProps = dispatch => {
     return {
         addNewTask: (task, token) => {
             dispatch(addTask(task, token));
+        },
+        mapUpdateTask: (task, token) => {
+            dispatch(updateTask(task, token));
         }
     }
 }
