0f71b9633362691b39cf2f12db5addc673f8b4ac
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-15 12:57:08 -0300

    Adicionando field de done no modelo task

--- todos/models.py
+++ todos/models.py
@@ -14,13 +14,14 @@ class Todo(models.Model):
 
 class Task(models.Model):
     name = models.CharField(max_length=100, blank=False, default=None)
-    deadline = models.DateField()
+    deadline = models.DateTimeField()
     user = models.ForeignKey(
         User, related_name='tasks', on_delete=models.CASCADE, blank=True,
         null=True)
     todo = models.ForeignKey(
         'Todo', related_name='tasks', on_delete=models.CASCADE, blank=False,
         null=False)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
