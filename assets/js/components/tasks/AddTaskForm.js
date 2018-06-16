17dd0ef88fe80e059de9cc990abce3dc3f453c42
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-15 21:45:15 -0300

    Atualizando componente de adicionar task com assign

--- assets/js/components/tasks/AddTaskForm.js
+++ assets/js/components/tasks/AddTaskForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { addTask } from '../../actions/tasks';
-import { Button } from 'react-bootstrap';
+import { getUsers } from '../../actions/user';
+import { Button, ButtonToolbar, DropdownButton, MenuItem } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { Link, browserHistory} from 'react-router';
 import { updateTask } from '../../actions/tasks';
@@ -8,15 +9,20 @@ import { updateTask } from '../../actions/tasks';
 
 class AddTaskForm extends React.Component {
     state = {
+        assign_title: 'Assign',
+        user: undefined,
         done: false,
         name: '',
         deadline: '',
-        task: {
-            name: ''
-        }
     }
 
-    componentWillMount() {
+    componentDidMount() {
+        this.setState(() => (this.props.task));
+        this.setState(() => ({
+            user: this.props.user
+        }));
+
+        this.props.mapGetUsers(this.props.token)
         if (this.props.task === undefined){
             this.setState(() => (
                 {name: ''}
@@ -27,17 +33,13 @@ class AddTaskForm extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault()
         if (this.props.task) {
-            this.props.mapUpdateTask({
-                done: this.state.done,
-                name: this.state.name,
-                id: this.props.task.id,
-                todo: this.props.todo.id,
-                deadline: this.state.deadline
-        }, this.props.token);
+            this.props.mapUpdateTask(
+                this.state, this.props.token);
         }else{
             this.props.addNewTask({
                 name: this.state.name,
                 todo: this.props.todo.id,
+                user: this.state.user,
                 deadline: this.state.deadline
             }, this.props.token);
         }
@@ -46,6 +48,22 @@ class AddTaskForm extends React.Component {
     updateValue = (e, data) => {
         const value = e.target.value;
         this.setState(() => (data));
+    };
+
+    generateUserList = () => {
+        return (
+            this.props.users.map(user =>
+                <MenuItem eventKey={user.id} key={user.id}
+                 onClick={(e) => {
+                    this.setState({
+                        assign_title: user.username,
+                        user: user.id,
+                    });
+                }}
+                >{user.username}
+                </MenuItem>
+            )
+        )
     };
 
     render() {
@@ -60,8 +78,16 @@ class AddTaskForm extends React.Component {
                 <input
                     onChange={(e) => {this.updateValue(e,
                     {deadline: e.target.value})}}
+                    value={this.state.deadline}
                     type="date"
                     />
+                <ButtonToolbar>
+                    <DropdownButton title={this.state.assign_title}
+                        id="dropdown-size-medium">
+                        {this.generateUserList()}
+                    </DropdownButton>
+                </ButtonToolbar>
+
                 <Button type="submit" onClick={this.handleSubmit}>Save</Button>
             </form>
         );
@@ -72,6 +98,9 @@ class AddTaskForm extends React.Component {
 const mapStateToProps = state => {
     return {
         token: state.authReducer.token,
+        users: state.userReducer.users,
+        user: state.authReducer.user,
+
     }
 }
 
@@ -82,6 +111,9 @@ const mapDispatchToProps = dispatch => {
         },
         mapUpdateTask: (task, token) => {
             dispatch(updateTask(task, token));
+        },
+        mapGetUsers: (token) => {
+            dispatch(getUsers(token));
         }
     }
 }
