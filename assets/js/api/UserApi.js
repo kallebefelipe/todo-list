044339d428873635f6cc17143f1a62336b8bee31
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-15 21:42:31 -0300

    Adicionando api de usuário

---
+++ assets/js/api/UserApi.js
@@ -0,0 +1,17 @@
+
+class UserApi {
+
+    static getAllUser(token) {
+        return fetch('api/user', {
+                headers: {
+                        'Authorization': `Token ${token}`,
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    }
+                },
+            ).then((response) =>  {
+                return response.json();
+            })
+    }
+}
+
+export default UserApi;
