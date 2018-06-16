1e319ee3fdbf457cc1d156e2bca09256f1fdd821
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-15 21:46:58 -0300

    Adicionando url de consulta de lista de usuários

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -8,14 +8,15 @@ from todos import views
 api_routers = routers.SimpleRouter()
 api_routers.register(r'todos', views.TodoViewSet)
 api_routers.register(r'tasks', views.TaskViewSet)
+api_routers.register(r'user', views.UserViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('docs/', include_docs_urls(title='Todo List')),
-    path('api/auth/register/', views.RegistrationView.as_view(),
-         name='register-user'),
+    path('', include(('frontend.urls', 'frontend'))),
     path('api/auth/login/', views.LoginView.as_view()),
     path('api/auth/', include('knox.urls')),
+    path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title='Todo List')),
     path('api/', include((api_routers.urls, 'todos', ))),
-    path('', include(('frontend.urls', 'frontend'))),
+    path('api/auth/register/', views.RegistrationView.as_view(),
+         name='register-user'),
 ]
