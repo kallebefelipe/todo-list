d4355218ae94595e6de528569cdada29e73bc393
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 07:55:52 -0300

    Refatoração ação autenticação

--- assets/js/actions/auth.js
+++ assets/js/actions/auth.js
@@ -1,51 +1,59 @@
 import authApi from '../api/AuthApi';
 
+
 export function userLoginSuccess(data) {
-    return {type: 'LOGIN_USER_SUCCESS', data};
+  return {type: 'LOGIN_USER_SUCCESS', data};
 }
+
 
 export function userRegisterSuccess(data) {
-    return {type: 'REGISTER_USER_SUCCESS', data};
+  return {type: 'REGISTER_USER_SUCCESS', data};
 }
+
 
 export function forgotPasswordSuccess(response) {
-    return {type: 'FORGOT_PASSWORD_SUCCESS', response};
+  return {type: 'FORGOT_PASSWORD_SUCCESS', response};
 }
+
 
 export function logoutSuccess(response) {
-    return {type: 'LOGOUT_SUCCESS', response};
+  return {type: 'LOGOUT_SUCCESS', response};
 }
 
+
 const registerUser = function (user) {
-    return function(dispatch) {
-        return authApi.registerUser(user).then((user) => {
-            dispatch(userLoginSuccess(user));
-        })
-    }
+  return function(dispatch) {
+    return authApi.registerUser(user).then((user) => {
+      dispatch(userLoginSuccess(user));
+    })
+  }
 };
+
 
 const loginUser = function (user) {
-    return function(dispatch) {
-        return authApi.loginUser(user).then((user) => {
-            dispatch(userLoginSuccess(user));
-        })
-    }
+  return function(dispatch) {
+    return authApi.loginUser(user).then((user) => {
+      dispatch(userLoginSuccess(user));
+    })
+  }
 };
+
 
 const forgotPassword = function (email) {
-    return function(dispatch) {
-        return authApi.forgotPassword(email).then((response) => {
-            dispatch(forgotPasswordSuccess(response));
-        })
-    }
+  return function(dispatch) {
+    return authApi.forgotPassword(email).then((response) => {
+      dispatch(forgotPasswordSuccess(response));
+    })
+  }
 };
 
+
 const logoutUser = function () {
-    return function(dispatch) {
-        return authApi.logoutUser().then((response) => {
-            dispatch(logoutSuccess(response));
-        })
-    }
+  return function(dispatch) {
+    return authApi.logoutUser().then((response) => {
+      dispatch(logoutSuccess(response));
+    })
+  }
 };
 
 
