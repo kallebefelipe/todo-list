4a5d5dc8ac01b0e4f8bf454955af285301d65fa1
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:31:51 -0300

    Atualizando componente de listagem de todos

--- assets/js/components/todos/TodoList.js
+++ assets/js/components/todos/TodoList.js
@@ -6,36 +6,33 @@ import { loadTodos } from '../../actions/todos';
 
 class TodoList extends React.Component {
 
-    componentDidMount() {
-        this.props.fetchTodos(this.props.token);
-    }
-    render () {
-        return (
-            <div>
-                <p>TODO-LISTS</p>
-                <ul>
-                    {this.props.todos ? this.props.todos.map(todo =>
-                        <TodoListRow key={todo.id} todo={todo} />
-                    ) : 'Loading...'}
-                </ul>
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.props.fetchTodos(this.props.token);
+  }
+
+  render () {
+    return (
+      <div className="main text-center">
+        {this.props.todos.map(todo =>
+          <TodoListRow key={todo.id} todo={todo} />)}
+       </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        todos: state.todoReducer.todos,
-        token: state.authReducer.token,
-    }
+  return {
+    todos: state.todoReducer.todos,
+    token: state.authReducer.token,
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        fetchTodos: (token) => {
-            dispatch(loadTodos(token));
-        }
+  return {
+    fetchTodos: (token) => {
+      dispatch(loadTodos(token));
     }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
