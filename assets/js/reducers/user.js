f6ae9f2a3c162c257582d377acb793e5b5f16dac
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 08:04:34 -0300

    Refatoração de reducer de user

--- assets/js/reducers/user.js
+++ assets/js/reducers/user.js
@@ -1,16 +1,19 @@
+
 const authState = {
     users: [],
 }
 
+
 const userReducer = (state= authState, action) => {
-    switch(action.type) {
-        case 'LOAD_USER_SUCCESS':
-            return {
-                users: action.users
-            };
-        default:
-            return state
-    }
+  switch(action.type) {
+    case 'LOAD_USER_SUCCESS':
+      return {
+        users: action.users
+      };
+    default:
+      return state
+  }
 }
+
 
 export default userReducer;
