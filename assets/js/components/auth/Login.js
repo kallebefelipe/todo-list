9332f145283cd4825bcc852d77b3cc7e9e9ef03d
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:23:27 -0300

    Atualiznado form de login

--- assets/js/components/auth/Login.js
+++ assets/js/components/auth/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+  import React from 'react';
 import Register from './Register';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/auth';
@@ -9,94 +9,90 @@ import {Link} from "react-router-dom";
 import { FormGroup, Form, Col, ControlLabel, FormControl, Checkbox, Button } from 'react-bootstrap';
 
 class Login extends React.Component {
-    state = {
-        username: '',
-        password: '',
-        token: '',
-        isAuthenticated: false
+  state = {
+    username: '',
+    password: '',
+    token: '',
+    isAuthenticated: false
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.mapLoginUser({
+      username: this.state.username,
+      password: this.state.password
+    });
+  }
+
+  render() {
+    if (this.props.isAuthenticated === true) {
+      return <Redirect to='/' />
     }
-
-    handleSubmit = (e) => {
-        e.preventDefault()
-        this.props.mapLoginUser({
-            username: this.state.username,
-            password: this.state.password
-        });
-    }
-
-    render() {
-        if (this.props.isAuthenticated === true) {
-            return <Redirect to='/app' />
-        }
-        return (
-            <div>
-
-                <Form horizontal>
-                    <FormGroup controlId="formHorizontalEmail">
-                        <Col componentClass={ControlLabel} sm={2}>
-                          Login
-                        </Col>
-                        <Col sm={10}>
-                          <FormControl
-                            onChange={(e) => {
-                            const value = e.target.value;
-                            this.setState(() => ({
-                                username: value
-                            }));
-                            }}
-                          type="login" placeholder="Login" />
-                        </Col>
-                    </FormGroup>
-
-                    <FormGroup controlId="formHorizontalPassword">
-                        <Col componentClass={ControlLabel} sm={2}>
-                          Password
-                        </Col>
-                        <Col sm={10}>
-                          <FormControl
-                            onChange={(e) => {
-                            const value = e.target.value;
-                            this.setState(() => ({
-                                password: value
-                            }));
-                           }}
-                           type="password" placeholder="Password" />
-                        </Col>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Col smOffset={2} sm={10}>
-                          <Checkbox>Remember me</Checkbox>
-                        </Col>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Col smOffset={2} sm={10}>
-                        <Button type="submit" onClick={this.handleSubmit}>Login
-                        </Button>
-                        </Col>
-                    </FormGroup>
-                    Don't have an account? <Link to="/register">Register</Link>
-                </Form>;
-            </div>
-
-        );
-    }
+    return (
+      <div>
+        <Form horizontal>
+        <FormGroup controlId="formHorizontalEmail">
+          <Col componentClass={ControlLabel} sm={2}>
+            Login
+          </Col>
+          <Col sm={10}>
+            <FormControl
+              onChange={(e) => {
+              const value = e.target.value;
+              this.setState(() => ({
+              username: value
+            }));
+          }}
+          type="login" placeholder="Login" />
+          </Col>
+        </FormGroup>
+        <FormGroup controlId="formHorizontalPassword">
+          <Col componentClass={ControlLabel} sm={2}>
+            Password
+          </Col>
+          <Col sm={10}>
+          <FormControl
+            onChange={(e) => {
+            const value = e.target.value;
+            this.setState(() => ({
+              password: value
+            }));
+          }}
+          type="password" placeholder="Password" />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+          <Checkbox>Remember me</Checkbox>
+          </Col>
+        </FormGroup>
+        <FormGroup>
+            <Col smOffset={2} sm={10}>
+            <Button type="submit" onClick={this.handleSubmit}>Login</Button>
+            <Button type="submit" onClick={(e) => (e.preventDefault())}>
+            <Link to="/register">Register</Link></Button>
+            <p>Forgot your <Link to="/forgot-password">password ?</Link></p>
+            </Col>
+          </FormGroup>
+        </Form>;
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = state => {
-    return {
-        isAuthenticated: state.authReducer.isAuthenticated
-    }
+  return {
+    isAuthenticated: state.authReducer.isAuthenticated
+  }
 }
 
 
 const mapDispatchToProps = dispatch => {
-    return {
-        mapLoginUser: (user) => {
-            dispatch(loginUser(user));
-        }
+  return {
+    mapLoginUser: (user) => {
+      dispatch(loginUser(user));
     }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
