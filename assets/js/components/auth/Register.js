2fbd65a0595376f31d78ada63aeed65f5b36b321
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:24:44 -0300

    Alteração de espaçamento do componente de Registro

--- assets/js/components/auth/Register.js
+++ assets/js/components/auth/Register.js
@@ -4,72 +4,65 @@ import { registerUser } from '../../actions/auth';
 import { Redirect} from 'react-router';
 
 class Register extends React.Component {
-    state = {
-        email: '',
-        username: '',
-        password: '',
-        token: '',
-        isAuthenticated: false
-    }
+  state = {
+    email: '',
+    username: '',
+    password: '',
+    token: '',
+    isAuthenticated: false
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        this.props.mapRegisterUser({
-            username: this.state.username,
-            password: this.state.password,
-            email: this.state.email,
-        });
-    }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.mapRegisterUser({
+      username: this.state.username,
+      password: this.state.password,
+      email: this.state.email,
+    });
+  }
 
-    render() {
-        if (this.props.isAuthenticated === true) {
-            return <Redirect to='/app' />
-        }
-        return (
-            <div>
-                <form>
-                    <input
-                        onChange={(e) => {
-                            const value = e.target.value;
-                            this.setState(() => ({
-                                email: value
-                            }));
-                        }} type="text" placeholder="email" />
-                    <input
-                        onChange={(e) => {
-                            const value = e.target.value;
-                            this.setState(() => ({
-                                username: value
-                            }));
-                        }} type="text" placeholder="Name" />
-                    <input
-                        onChange={(e) => {
-                            const value = e.target.value;
-                            this.setState(() => ({
-                                password: value
-                            }));
-                        }} type="password" placeholder="Password" />
-                    <button type="submit" onClick={this.handleSubmit}>Register
-                    </button>
-                </form>
-            </div>
-        );
+  render() {
+    if (this.props.isAuthenticated === true) {
+      return <Redirect to='/' />
+    }
+    return (
+      <div>
+        <form>
+          <input
+            onChange={(e) => {
+              const value = e.target.value;
+              this.setState(() => ({email: value}));
+            }}
+            type="text" placeholder="email" />
+          <input
+            onChange={(e) => {
+              const value = e.target.value;
+              this.setState(() => ({username: value}));
+            }} type="text" placeholder="Name" />
+          <input
+            onChange={(e) => {
+              const value = e.target.value;
+              this.setState(() => ({password: value}));
+            }} type="password" placeholder="Password" />
+          <button type="submit" onClick={this.handleSubmit}>Register</button>
+        </form>
+      </div>
+      );
     }
 };
 
 const mapStateToProps = state => {
-    return {
-        isAuthenticated: state.authReducer.isAuthenticated
-    }
+  return {
+    isAuthenticated: state.authReducer.isAuthenticated
+  }
 }
 
-
 const mapDispatchToProps = dispatch => {
-    return {
-        mapRegisterUser: (user) => {
-            dispatch(registerUser(user));
-        }
-    }
+  return {
+    mapRegisterUser: (user) => {
+      dispatch(registerUser(user));
+      }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Register);
