95a3938b6f1d4f7f29642584890f1fb97bfc9e43
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:12:56 -0300

    Adicionando ação de logout e esqueci senha

--- assets/js/actions/auth.js
+++ assets/js/actions/auth.js
@@ -8,6 +8,14 @@ export function userRegisterSuccess(data) {
     return {type: 'REGISTER_USER_SUCCESS', data};
 }
 
+export function forgotPasswordSuccess(response) {
+    return {type: 'FORGOT_PASSWORD_SUCCESS', response};
+}
+
+export function logoutSuccess(response) {
+    return {type: 'LOGOUT_SUCCESS', response};
+}
+
 const registerUser = function (user) {
     return function(dispatch) {
         return authApi.registerUser(user).then((user) => {
@@ -24,5 +32,21 @@ const loginUser = function (user) {
     }
 };
 
+const forgotPassword = function (email) {
+    return function(dispatch) {
+        return authApi.forgotPassword(email).then((response) => {
+            dispatch(forgotPasswordSuccess(response));
+        })
+    }
+};
 
-export { registerUser, loginUser};
+const logoutUser = function () {
+    return function(dispatch) {
+        return authApi.logoutUser().then((response) => {
+            dispatch(logoutSuccess(response));
+        })
+    }
+};
+
+
+export { registerUser, loginUser, forgotPassword, logoutUser};
