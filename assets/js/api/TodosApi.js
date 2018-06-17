a59403f903fe315af7c139bedbca2253d5b10eba
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 07:58:04 -0300

    Refatoração api de todos

--- assets/js/api/TodosApi.js
+++ assets/js/api/TodosApi.js
@@ -1,60 +1,56 @@
+
 class TodosApi {
-    static getAllTodos(token) {
-        return fetch('/api/todos/', {
-                headers: {
-                        'Authorization': `Token ${token}`,
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                }
 
-            ).then((response) => response.json()
-            ).then((responseData) => {
-                return responseData;
-            });
+  static getAllTodos(token) {
+    return fetch('/api/todos/', {
+      headers: {
+        'Authorization': `Token ${token}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }).then((response) => response.json()).then((responseData) => {
+          return responseData;
+      });
     }
 
-    static deleteTodo(data, token) {
-        return fetch(`/api/todos/${data.id}/`, {
-                method: 'DELETE',
-                headers: {
-                        'Authorization': `Token ${token}`,
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                }
+  static deleteTodo(data, token) {
+    return fetch(`/api/todos/${data.id}/`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token ${token}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }).then((response) =>  {
+        return response.status;
+    })
+  }
 
-            ).then((response) =>  {
-                return response.status;
-            })
-    }
+  static addTodo(data, token) {
+    return fetch('/api/todos/', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Token ${token}`,
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        },
+      body: JSON.stringify(data)
+      }).then((response) =>  {
+        return response.json();
+    })
+  }
 
-    static addTodo(data, token) {
-        return fetch('/api/todos/', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Token ${token}`,
-                    'Accept': 'application/json',
-                    'Content-type': 'application/json',
-                },
-                body: JSON.stringify(data)
-            }).then((response) =>  {
-                return response.json();
-            })
-    }
-
-    static updateTodo(data, token) {
-        return fetch(`api/todos/${data.todo.id}/`, {
-                method: 'PUT',
-                headers: {
-                        'Authorization': `Token ${token}`,
-                        'Content-type': 'application/json',
-                    },
-                body: JSON.stringify(data.todo)
-                },
-
-            ).then((response) =>  {
-                return response.status;
-            })
-    }
+  static updateTodo(data, token) {
+    return fetch(`api/todos/${data.todo.id}/`, {
+      method: 'PUT',
+      headers: {
+              'Authorization': `Token ${token}`,
+              'Content-type': 'application/json',
+          },
+      body: JSON.stringify(data.todo)
+      }).then((response) =>  {
+        return response.status;
+    })
+  }
 }
+
 
 export default TodosApi;
