2e92a9f5a3e0dda229b93fdc6ae4d8fd63c1e8a6
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:32:58 -0300

    Adicionando ação de logout e esqueci senha no reducer de autenticação

--- assets/js/reducers/auth.js
+++ assets/js/reducers/auth.js
@@ -1,7 +1,7 @@
 const authState = {
     username: '',
     email: '',
-    token: '',
+    token: localStorage.getItem("token"),
     user: undefined,
     isAuthenticated: false,
 }
@@ -13,12 +13,14 @@ const userInfo = (action) => {
         username: action.data.user.username,
         user: action.data.user.id,
         isAuthenticated: true,
+        status: action.status_code
     };
 }
 
 const authReducer = (state= authState, action) => {
     switch(action.type) {
         case 'REGISTER':
+            localStorage.setItem("token", action.data.token);
             return {
                 token: action.token,
                 email: action.email,
@@ -26,9 +28,22 @@ const authReducer = (state= authState, action) => {
             };
             break;
         case 'LOGIN_USER_SUCCESS':
+            localStorage.setItem("token", action.data.token);
             return userInfo(action);
         case 'REGISTER_USER_SUCCESS':
             return userInfo(action);
+        case 'FORGOT_PASSWORD_SUCCESS':
+            return userInfo(action);
+        case 'LOGOUT_SUCCESS':
+            localStorage.removeItem("token");
+            return {
+                token: '',
+                email: '',
+                username: '',
+                user: '',
+                isAuthenticated: false,
+                status: action.status_code
+            }
         default:
             return state
     }
