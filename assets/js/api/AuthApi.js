93c330c27a5c5637e8b3c2c11e3ca0dc2a5a572c
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 07:57:27 -0300

    Refatoração api autenticação

--- assets/js/api/AuthApi.js
+++ assets/js/api/AuthApi.js
@@ -1,82 +1,62 @@
-
-function getCookie(name) {
-    if (!document.cookie) {
-      return null;
-    }
-    const token = document.cookie.split(';')
-      .map(c => c.trim())
-      .filter(c => c.startsWith(name + '='));
-
-    if (token.length === 0) {
-      return null;
-    }
-    return decodeURIComponent(token[0].split('=')[1]);
-  }
+import getCookie from '../common/helpers'
 
 
 class AuthApi {
 
-    static loginUser(data) {
-        return fetch('api/auth/login/', {
-                method: 'POST',
-                headers: {
-                        'Content-type': 'application/json',
-                    },
-                body: JSON.stringify(data)
-                },
-
-            ).then((response) =>  {
-                return response.json();
-            })
+  static loginUser(data) {
+    return fetch('api/auth/login/', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        },
+      body: JSON.stringify(data)
+      }).then((response) =>  {
+        return response.json();
+      })
     }
 
-    static registerUser(data) {
-        return fetch('api/auth/register/', {
-                method: 'POST',
-                headers: {
-                        'Content-type': 'application/json',
-                    },
-                body: JSON.stringify(data)
-                },
-
-            ).then((response) =>  {
-                return response.json();
-            })
+  static registerUser(data) {
+    return fetch('api/auth/register/', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        },
+      body: JSON.stringify(data)
+      }).then((response) =>  {
+      return response.json();
+      })
     }
 
-    static forgotPassword(email) {
-        const csrf = getCookie('csrftoken')
-        return fetch('/rest-auth/password/reset/', {
-                method: 'POST',
-                mode: 'same-origin',
-                credentials: 'include',
-                headers: {
-                        'Accept': 'application/json',
-                        'Content-type': 'application/json',
-                        'X-CSRFToken': csrf
-                    },
-                body: JSON.stringify({
-                    email: email
-                })
-                },
-
-            ).then((response) =>  {
-                return response.json();
-            })
+  static forgotPassword(email) {
+    const csrf = getCookie('csrftoken')
+    return fetch('/rest-auth/password/reset/', {
+      method: 'POST',
+      mode: 'same-origin',
+      credentials: 'include',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        'X-CSRFToken': csrf
+        },
+      body: JSON.stringify({
+      email: email
+        })
+      }).then((response) =>  {
+        return response.json();
+      })
     }
 
-    static logoutUser() {
-        return fetch('rest-auth/logout/ ', {
-                method: 'POST',
-                headers: {
-                        'Content-type': 'application/json',
-                    },
-                },
-
-            ).then((response) =>  {
-                return response.json();
-            })
+  static logoutUser() {
+    return fetch('rest-auth/logout/ ', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        },
+      }).then((response) =>  {
+        return response.json();
+      })
     }
 }
+
 
 export default AuthApi;
