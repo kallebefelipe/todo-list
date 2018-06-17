3b4b7ce8682c6b59821addb5c052ce056a8c7581
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:17:36 -0300

    Adicionando api de pegar todos usuários

--- assets/js/api/UserApi.js
+++ assets/js/api/UserApi.js
@@ -2,7 +2,7 @@
 class UserApi {
 
     static getAllUser(token) {
-        return fetch('api/user', {
+        return fetch('api/user/', {
                 headers: {
                         'Authorization': `Token ${token}`,
                         'Content-Type': 'application/x-www-form-urlencoded',
