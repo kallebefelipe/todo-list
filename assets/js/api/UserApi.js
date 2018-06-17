e2f9f7655eb0bf63e19b700b1ebbaaffea510a5c
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 07:58:24 -0300

    Refatoração de api de usuário

--- assets/js/api/UserApi.js
+++ assets/js/api/UserApi.js
@@ -1,17 +1,17 @@
 
 class UserApi {
 
-    static getAllUser(token) {
-        return fetch('api/user/', {
-                headers: {
-                        'Authorization': `Token ${token}`,
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    }
-                },
-            ).then((response) =>  {
-                return response.json();
-            })
+  static getAllUser(token) {
+    return fetch('api/user/', {
+      headers: {
+        'Authorization': `Token ${token}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    }).then((response) =>  {
+      return response.json();
+      })
     }
 }
+
 
 export default UserApi;
