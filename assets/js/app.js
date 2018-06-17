b7e607aba287d3f227c8984484b8caa75cceabd3
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:21:27 -0300

    Refatoração de rotas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,19 @@
 import 'normalize.css/normalize.css';
-import AddTaskForm from './components/tasks/AddTaskForm';
-import Login from './components/auth/Login';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Register from './components/auth/Register';
 import setUpStore from './store/configureStore';
-import TodoApp from './components/TodoApp';
 import { loadTodos } from './actions/todos';
 import { Provider } from 'react-redux';
-import { Route, Switch, BrowserRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
+import Jsx from './components/routers';
+
 
 const store = setUpStore();
 
-const Jsx = () => (
-    <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path='/' component={Login} />
-                <Route path='/add_task' component={AddTaskForm} />
-                <Route path='/app' component={TodoApp} />
-                <Route exact path='/register' component={Register} />
-            </Switch>
-        </BrowserRouter>
-    </Provider>
-);
+const Template = () => (
+  <Provider store={store}>
+    <Jsx />
+  </Provider>
+)
 
-ReactDOM.render(<Jsx />, document.getElementById('app'));
+ReactDOM.render(<Template />, document.getElementById('app'));
