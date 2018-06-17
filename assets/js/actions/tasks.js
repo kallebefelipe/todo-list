e8ec025ec1b82c4caa4b165ad3f7974ba2a77a05
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 07:56:10 -0300

    Refatoração ação tasks

--- assets/js/actions/tasks.js
+++ assets/js/actions/tasks.js
@@ -1,48 +1,53 @@
 import taskApi from '../api/TasksApi';
 
+
 export function addTaskSuccess(data) {
-    return {type: 'ADD_TASK_SUCCESS', data};
+  return {type: 'ADD_TASK_SUCCESS', data};
 }
+
 
 export function updateTaskSuccess(data) {
-    return {
-        type: 'UPDATE_TASK_SUCCESS',
-        task: data
-    };
+  return {
+    type: 'UPDATE_TASK_SUCCESS',
+    task: data
+  };
 }
+
 
 export function deleteTaskSuccess(data) {
-    return {
-        type: 'DELETE_TASK_SUCCESS',
-        task: data.task,
-        todo_id: data.todo.id,
-    };
+  return {
+    type: 'DELETE_TASK_SUCCESS',
+    task: data.task,
+    todo_id: data.todo.id,
+  };
 }
 
+
 const addTask = function (task, token) {
-    console.log(task)
-    return function(dispatch) {
-        return taskApi.addTask(task, token).then((newTask) => {
-            dispatch(addTaskSuccess(newTask));
-        })
-    }
+  return function(dispatch) {
+    return taskApi.addTask(task, token).then((newTask) => {
+      dispatch(addTaskSuccess(newTask));
+    })
+  }
 };
 
+
 const deleteTask = (task, token) => {
-    return (dispatch) => {
-        return taskApi.deleteTask(task, token).then(() => {
-            dispatch(deleteTaskSuccess(task));
-        })
-    }
+  return (dispatch) => {
+    return taskApi.deleteTask(task, token).then(() => {
+      dispatch(deleteTaskSuccess(task));
+    })
+  }
 };
 
 
 const updateTask = (task, token) => {
-    return (dispatch) => {
-        return taskApi.updateTask(task, token).then((task) => {
-            dispatch(updateTaskSuccess(task));
-        })
-    }
+  return (dispatch) => {
+    return taskApi.updateTask(task, token).then((task) => {
+      dispatch(updateTaskSuccess(task));
+    })
+  }
 };
+
 
 export { addTask, deleteTask, updateTask };
