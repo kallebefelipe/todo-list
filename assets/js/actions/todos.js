950376eae338c5e7dab8397bcfc6977d535499d2
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 07:56:33 -0300

    Refatoração ação user

--- assets/js/actions/todos.js
+++ assets/js/actions/todos.js
@@ -1,56 +1,66 @@
 import todoApi from '../api/TodosApi';
 
+
 export function deleteTodoSuccess(todo) {
-    return {type: 'DELETE_TODO_SUCCESS', todo};
+  return {type: 'DELETE_TODO_SUCCESS', todo};
 }
+
 
 export function addTodoSuccess(todo) {
-    return {type: 'ADD_TODO_SUCCESS', todo};
+  return {type: 'ADD_TODO_SUCCESS', todo};
 }
+
 
 export function loadTodoSuccess(todos) {
-    return {type: 'LOAD_TODO_SUCCESS', todos};
+  return {type: 'LOAD_TODO_SUCCESS', todos};
 }
+
 
 export function updateTodoSuccess(todo) {
-    return {type: 'UPDATE_TODO_SUCCESS', todo};
+  return {type: 'UPDATE_TODO_SUCCESS', todo};
 }
 
+
 const populateInitialState = (data) => ({
-    type: 'POPULATE_INITIAL_STATE',
-    data: data
+  type: 'POPULATE_INITIAL_STATE',
+  data: data
 });
 
+
 const loadTodos = (token) => {
-    return (dispatch) => {
-        return todoApi.getAllTodos(token).then(todos => {
-            dispatch(loadTodoSuccess(todos));
-        });
-    }
+  return (dispatch) => {
+    return todoApi.getAllTodos(token).then(todos => {
+      dispatch(loadTodoSuccess(todos));
+    });
+  }
 };
+
 
 const deleteTodo = (todo, token) => {
-    return (dispatch) => {
-        return todoApi.deleteTodo(todo, token).then(() => {
-            dispatch(deleteTodoSuccess(todo));
-        })
-    }
+  return (dispatch) => {
+    return todoApi.deleteTodo(todo, token).then(() => {
+      dispatch(deleteTodoSuccess(todo));
+    })
+  }
 };
+
 
 const addTodo = function (todo, token) {
-    return function(dispatch) {
-        return todoApi.addTodo(todo, token).then((newTodo) => {
-            dispatch(addTodoSuccess(newTodo));
-        })
-    }
+  return function(dispatch) {
+    return todoApi.addTodo(todo, token).then((newTodo) => {
+      dispatch(addTodoSuccess(newTodo));
+    })
+  }
 };
 
+
 const updateTodo = (todo, token) => {
-    return (dispatch) => {
-        return todoApi.updateTodo(todo, token).then(() => {
-            dispatch(updateTodoSuccess(todo));
-        })
-    }
+  return (dispatch) => {
+    return todoApi.updateTodo(todo, token).then(() => {
+      dispatch(updateTodoSuccess(todo));
+    })
+  }
 };
+
 
 export { populateInitialState, addTodo, deleteTodo, loadTodos, updateTodo };
