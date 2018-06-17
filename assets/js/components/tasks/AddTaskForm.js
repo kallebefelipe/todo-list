86bee99a642db44486eb2ce3e9f1daee4bdab1d8
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 08:01:07 -0300

    Refatoração de componente de adicionar task

--- assets/js/components/tasks/AddTaskForm.js
+++ assets/js/components/tasks/AddTaskForm.js
@@ -8,114 +8,108 @@ import { updateTask } from '../../actions/tasks';
 
 
 class AddTaskForm extends React.Component {
-    state = {
-        assign_title: 'Assign',
-        user: undefined,
-        done: false,
-        name: '',
-        deadline: '',
+  state = {
+    assign_title: 'Assign',
+    user: undefined,
+    done: false,
+    name: '',
+    deadline: '',
+  }
+
+  componentDidMount() {
+    this.setState(() => (this.props.task));
+    this.setState(() => ({user: this.props.user}));
+
+    this.props.mapGetUsers(this.props.token)
+    if (this.props.task === undefined){
+      this.setState(() => ({name: ''}));
     }
+  }
 
-    componentDidMount() {
-        this.setState(() => (this.props.task));
-        this.setState(() => ({
-            user: this.props.user
-        }));
-
-        this.props.mapGetUsers(this.props.token)
-        if (this.props.task === undefined){
-            this.setState(() => (
-                {name: ''}
-            ));
-        }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.props.task) {
+      this.props.mapUpdateTask(this.state, this.props.token);
+    }else{
+      this.props.addNewTask({
+        name: this.state.name,
+        todo: this.props.todo.id,
+        user: this.state.user,
+        deadline: this.state.deadline},
+        this.props.token);
     }
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        if (this.props.task) {
-            this.props.mapUpdateTask(
-                this.state, this.props.token);
-        }else{
-            this.props.addNewTask({
-                name: this.state.name,
-                todo: this.props.todo.id,
-                user: this.state.user,
-                deadline: this.state.deadline
-            }, this.props.token);
-        }
-    };
+  updateValue = (e, data) => {
+    const value = e.target.value;
+    this.setState(() => (data));
+  };
 
-    updateValue = (e, data) => {
-        const value = e.target.value;
-        this.setState(() => (data));
-    };
+  generateUserList = () => {
+    return (
+      this.props.users.map(user =>
+        <MenuItem
+          eventKey={user.id}
+          key={user.id}
+          onClick={(e) => {this.setState({assign_title: user.username, user: user.id});}}>
+          {user.username}
+        </MenuItem>
+      )
+    )
+  };
 
-    generateUserList = () => {
-        return (
-            this.props.users.map(user =>
-                <MenuItem eventKey={user.id} key={user.id}
-                 onClick={(e) => {
-                    this.setState({
-                        assign_title: user.username,
-                        user: user.id,
-                    });
-                }}
-                >{user.username}
-                </MenuItem>
-            )
-        )
-    };
+  render() {
+    return (
+      <form>
+        <input
+          onChange={(e) => {this.updateValue(e, {name: e.target.value})}}
+          type="text" placeholder="Name"
+          value={this.state.name}
+          />
 
-    render() {
-        return (
-            <form>
-                <input
-                    onChange={(e) => {this.updateValue(e,
-                    {name: e.target.value})}}
-                    type="text" placeholder="Name"
-                    value={this.state.name}
-                    />
-                <input
-                    onChange={(e) => {this.updateValue(e,
-                    {deadline: e.target.value})}}
-                    value={this.state.deadline}
-                    type="date"
-                    />
-                <ButtonToolbar>
-                    <DropdownButton title={this.state.assign_title}
-                        id="dropdown-size-medium">
-                        {this.generateUserList()}
-                    </DropdownButton>
-                </ButtonToolbar>
+        <input
+          onChange={(e) => {this.updateValue(e, {deadline: e.target.value})}}
+          value={this.state.deadline}
+          type="date"
+          />
 
-                <Button type="submit" onClick={this.handleSubmit}>Save</Button>
-            </form>
-        );
-    }
+        <ButtonToolbar>
+          <DropdownButton
+            title={this.state.assign_title}
+            id="dropdown-size-medium">
+            {this.generateUserList()}
+          </DropdownButton>
+        </ButtonToolbar>
 
+        <Button type="submit" onClick={this.handleSubmit}>Save</Button>
+      </form>
+    );
+  }
 };
 
-const mapStateToProps = state => {
-    return {
-        token: state.authReducer.token,
-        users: state.userReducer.users,
-        user: state.authReducer.user,
 
-    }
+const mapStateToProps = state => {
+  return {
+    token: state.authReducer.token,
+    users: state.userReducer.users,
+    user: state.authReducer.user,
+  }
 }
+
 
 const mapDispatchToProps = dispatch => {
-    return {
-        addNewTask: (task, token) => {
-            dispatch(addTask(task, token));
-        },
-        mapUpdateTask: (task, token) => {
-            dispatch(updateTask(task, token));
-        },
-        mapGetUsers: (token) => {
-            dispatch(getUsers(token));
-        }
+  return {
+    addNewTask: (task, token) => {
+      dispatch(addTask(task, token));
+    },
+    mapUpdateTask: (task, token) => {
+      dispatch(updateTask(task, token));
+    },
+    mapGetUsers: (token) => {
+      dispatch(getUsers(token));
     }
+  }
 }
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddTaskForm);
