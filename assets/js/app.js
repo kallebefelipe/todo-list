ec884088a80c097a4b59acd8acaae42ee2aba43a
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 07:58:38 -0300

    Refatoração de app

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,10 +10,12 @@ import Jsx from './components/routers';
 
 const store = setUpStore();
 
+
 const Template = () => (
   <Provider store={store}>
     <Jsx />
   </Provider>
 )
+
 
 ReactDOM.render(<Template />, document.getElementById('app'));
