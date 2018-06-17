87e74a9b2c23cb0345b1b97d1ec068c7c1bec217
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:32:17 -0300

    Atualizando componente de TodoListRow

--- assets/js/components/todos/TodoListRow.js
+++ assets/js/components/todos/TodoListRow.js
@@ -10,104 +10,98 @@ import Popup from "reactjs-popup";
 
 
 class TodoListRow extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            data: undefined,
-            newName: undefined,
-            update: false,
-        };
-        this.subDeleteTodo = this.subDeleteTodo.bind(this);
-        this.toEditTodo = this.toEditTodo.bind(this);
-        this.subDeleteTodo = this.subDeleteTodo.bind(this);
-        this.editForm = this.editForm.bind(this);
+  constructor(props, context) {
+    super(props, context);
+      this.state = {
+        data: undefined,
+        newName: undefined,
+        update: false,
+      };
+    this.subDeleteTodo = this.subDeleteTodo.bind(this);
+    this.toEditTodo = this.toEditTodo.bind(this);
+    this.subDeleteTodo = this.subDeleteTodo.bind(this);
+    this.editForm = this.editForm.bind(this);
     }
 
-    subDeleteTodo(event) {
-        this.props.mapDeleteTodo(
-            this.props.todo,
-            this.props.token
-        );
-    }
+  subDeleteTodo(event) {
+      this.props.mapDeleteTodo(
+      this.props.todo,
+      this.props.token
+    );
+  }
 
-    addTask(event) {
-        this.props.mapDeleteTodo(
-            this.props.todo,
-            this.props.token
-        );
-    }
+  addTask(event) {
+    this.props.mapDeleteTodo(
+      this.props.todo,
+      this.props.token
+    );
+  }
 
-    toEditTodo(event) {
-        this.setState(() => ({
-            update: true
-        }));
-    }
+  toEditTodo(event) {
+    this.setState(() => ({
+      update: true
+    }));
+  }
 
-    subUpdateTodo = (event) => {
-        this.props.todo.name = this.state.newName;
-        this.props.mapUpdateTodo({
-            todo: this.props.todo}, this.props.token);
-        this.setState(() => ({
-            update: false
-        }));
-    };
+  subUpdateTodo = (event) => {
+    this.props.todo.name = this.state.newName;
+    this.props.mapUpdateTodo({todo: this.props.todo}, this.props.token);
+    this.setState(() => ({update: false}));
+  };
 
-    editForm(event) {
-        return <div> {
-            this.state.update ?
-                <div>
-                <input onChange={(e) => {
-                    const value = e.target.value;
-                    this.setState(() => ({
-                        newName: value
-                    }));
-                }} type="text" placeholder="Name" />
-                <Button type="submit" onClick={this.subUpdateTodo}>Ok</Button>
-                </div>
-                :
-                <Button type="submit" onClick={this.toEditTodo}>Edit</Button>
-            }
+  editForm(event) {
+    if (this.state.update) {
+      return (
+        <div>
+          <input onChange={(e) => {
+            const value = e.target.value;
+            this.setState(() => ({newName: value}));
+            }} type="text" placeholder="Name" />
+          <Button type="submit" onClick={this.subUpdateTodo}>Ok</Button>
         </div>
+      );
     }
+    return (
+      <Button type="submit" onClick={this.toEditTodo}>Edit</Button>
+    )
+  }
 
-    render() {
-        return (
+  render() {
+    return (
+      <div>
+        <h2>{this.props.todo.name}</h2>
+        <Button type="submit" onClick={this.subDeleteTodo}>Remove</Button>
+        {this.editForm()}
+        <Popup trigger={<Button>New Task</Button>} position="top left">
+          {close => (
             <div>
-                {this.props.todo.name}
-                <Button type="submit" onClick={this.subDeleteTodo}>Remove</Button>
-                {this.editForm()}
-                <TaskList tasks={this.props.todo.tasks} todo={this.props.todo} />
-                    <Popup trigger={<Button>New Task</Button>} position="top left">
-                    {close => (
-                      <div>
-                        <AddTaskForm todo={this.props.todo} />
-                        <a className="close" onClick={close}>
-                          Cancel
-                        </a>
-                      </div>
-                    )}
-              </Popup>
+              <AddTaskForm todo={this.props.todo} />
+              <a className="close" onClick={close}>Cancel</a>
             </div>
-        )
-    }
+          )}
+        </Popup>
+        <TaskList tasks={this.props.todo.tasks} todo={this.props.todo} />
+      </div>
+    )
+  }
 };
 
 const mapStateToProps = state => {
-    return {
-        todos: state.todoReducer.todos,
-        token: state.authReducer.token,
-    }
+  return {
+    todos: state.todoReducer.todos,
+    token: state.authReducer.token,
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        mapDeleteTodo: (todo, token) => {
-            dispatch(deleteTodo(todo, token));
-        },
-        mapUpdateTodo: (todo, token) => {
-            dispatch(updateTodo(todo, token));
-        }
+  return {
+    mapDeleteTodo: (todo, token) => {
+      dispatch(deleteTodo(todo, token));
+    },
+    mapUpdateTodo: (todo, token) => {
+      dispatch(updateTodo(todo, token));
     }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoListRow);
