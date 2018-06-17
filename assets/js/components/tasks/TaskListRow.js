f9cf05d397deeb3486e8e37f669cedee050cbbe9
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:27:56 -0300

    Atualizando html do componente task, para estilização

--- assets/js/components/tasks/TaskListRow.js
+++ assets/js/components/tasks/TaskListRow.js
@@ -2,64 +2,62 @@ import AddTaskForm from '../tasks/AddTaskForm';
 import Popup from "reactjs-popup";
 import React from 'react';
 import { connect } from 'react-redux';
-import { updateTask } from '../../actions/tasks';
+import { updateTask, deleteTask } from '../../actions/tasks';
 import { Button } from 'react-bootstrap';
 
 
 class TaskListRow extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            data: undefined,
-            newName: undefined,
-            update: false,
-        };
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      data: undefined,
+      newName: undefined,
+      update: false,
+    };
+  }
+
+  deleteTask = (event) => {
+    this.props.mapDeleteTask({
+        task: this.props.task,
+        todo: this.props.todo
+      },
+      this.props.token);
     }
 
-    deleteTask = (event) => {
-        this.props.propDeleteTask({
-            task: this.props.task,
-            todo: this.props.todo,
+  editForm = (event) => {
+    return <div className="edit-task">
+      <Popup trigger={<Button>Edit</Button>} position="top left">
+        {close => (
+          <div>
+            <AddTaskForm todo={this.props.todo} task={this.props.task} />
+            <a className="close" onClick={close}>Cancel</a>
+          </div>
+        )}
+      </Popup>
+    </div>
+  }
 
-        }, this.props.token);
-    }
+  updateDoneTask = (event) => {
+    this.props.task.done = !this.props.task.done;
+    const task = this.props.task;
+    this.props.mapUpdateTask(task, this.props.token);
+  }
 
-    editForm = (event) => {
-        return <div>
-            <Popup trigger={<Button>Edit Task</Button>} position="top left">
-                {close => (
-                  <div>
-                    <AddTaskForm todo={this.props.todo} task={this.props.task} />
-                    <a className="close" onClick={close}>
-                      Cancel
-                    </a>
-                  </div>
-                )}
-              </Popup>
-
-        </div>
-    }
-
-    updateDoneTask = (event) => {
-        this.props.task.done = !this.props.task.done;
-        const task = this.props.task;
-        this.props.mapUpdateTask(task, this.props.token);
-    }
-
-    render() {
-        return (
-            <div>
-                <p>{this.props.task.name}</p>
-                    <input type="checkbox"
-                      onChange={(e) => {
-                        const value = e.target.value;
-                        this.updateDoneTask(value);
-                    }} />Done
-                    <Button type="submit" onClick={this.deleteTask}>Remove</Button>
-                    {this.editForm()}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <li className="responded"><span>{this.props.task.name}</span>
+        <label>
+          <input type="checkbox"
+            onChange={(e) => {
+              const value = e.target.value;
+              this.updateDoneTask(value);
+          }}/>Done
+        </label>
+        <Button type="submit" onClick={(e) => this.deleteTask(e)}>Remove</Button>
+        {this.editForm()}
+      </li>
+    )
+  }
 };
 
 const mapStateToProps = state => {
@@ -71,11 +69,14 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         addNewTask: (task, token) => {
-            dispatch(addTask(task, token));
+          dispatch(addTask(task, token));
         },
         mapUpdateTask: (task, token) => {
-            dispatch(updateTask(task, token));
-        }
+          dispatch(updateTask(task, token));
+        },
+        mapDeleteTask: (task, token) => {
+          dispatch(deleteTask(task,token))
+        },
     }
 }
 
