0374cc63b1e933ed735026ef71e7b4badeacb5c2
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:28:46 -0300

    Atualizando html do componente de adicionar todo, para estilização, e alteração de espaçamentos

--- assets/js/components/todos/AddTodoForm.js
+++ assets/js/components/todos/AddTodoForm.js
@@ -5,48 +5,46 @@ import { connect } from 'react-redux';
 
 
 class AddTodoForm extends React.Component {
-    state = {
-        inputValue: ''
-    }
+  state = {
+    inputValue: ''
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.addNewTodo({
-            name: this.state.inputValue,
-            tasks: this.state.tasks,
-        }, this.props.token);
-    };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addNewTodo({
+      name: this.state.inputValue,
+      tasks: this.state.tasks},
+      this.props.token);
+  };
 
-    render() {
-        return (
-            <form>
-                <input
-                    onChange={(e) => {
-                        const value = e.target.value;
-                        this.setState(() => ({
-                            inputValue: value
-                        }));
-                    }} type="text" placeholder="Name" />
-                <Button type="button" className="btn btn-outline-dark"
-                 onClick={this.handleSubmit}>Add todo list</Button>
-            </form>
-        );
-    }
-
+  render() {
+    return (
+      <form className='submit-todo'>
+        <input
+          onChange={(e) => {
+            const value = e.target.value;
+            this.setState(() => ({inputValue: value}));
+            }}
+            type="text" placeholder="Name" />
+        <Button type="button" className="btn btn-outline-dark"
+          onClick={this.handleSubmit}>Add todo List</Button>
+      </form>
+    );
+  }
 };
 
 const mapStateToProps = state => {
-    return {
-        token: state.authReducer.token,
-    }
+  return {
+    token: state.authReducer.token,
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        addNewTodo: (todo, token) => {
-            dispatch(addTodo(todo, token));
-        }
+  return {
+    addNewTodo: (todo, token) => {
+      dispatch(addTodo(todo, token));
     }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddTodoForm);
