1608db8b29d357d0f53e08c7241d485f77247d74
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 08:04:00 -0300

    Refatoração de reducer de autenticação

--- assets/js/reducers/auth.js
+++ assets/js/reducers/auth.js
@@ -1,52 +1,56 @@
+
 const authState = {
-    username: '',
-    email: '',
-    token: localStorage.getItem("token"),
-    user: undefined,
-    isAuthenticated: false,
+  username: '',
+  email: '',
+  token: localStorage.getItem("token"),
+  user: undefined,
+  isAuthenticated: false,
 }
+
 
 const userInfo = (action) => {
-    return {
-        token: action.data.token,
-        email: action.data.user.email,
-        username: action.data.user.username,
-        user: action.data.user.id,
-        isAuthenticated: true,
-        status: action.status_code
-    };
+  return {
+    token: action.data.token,
+    email: action.data.user.email,
+    username: action.data.user.username,
+    user: action.data.user.id,
+    isAuthenticated: true,
+    status: action.status_code
+  };
 }
 
+
 const authReducer = (state= authState, action) => {
-    switch(action.type) {
-        case 'REGISTER':
-            localStorage.setItem("token", action.data.token);
-            return {
-                token: action.token,
-                email: action.email,
-                username: action.username,
-            };
-            break;
-        case 'LOGIN_USER_SUCCESS':
-            localStorage.setItem("token", action.data.token);
-            return userInfo(action);
-        case 'REGISTER_USER_SUCCESS':
-            return userInfo(action);
-        case 'FORGOT_PASSWORD_SUCCESS':
-            return userInfo(action);
-        case 'LOGOUT_SUCCESS':
-            localStorage.removeItem("token");
-            return {
-                token: '',
-                email: '',
-                username: '',
-                user: '',
-                isAuthenticated: false,
-                status: action.status_code
-            }
-        default:
-            return state
-    }
+  switch(action.type) {
+    case 'REGISTER':
+      localStorage.setItem("token", action.data.token);
+      return {
+        token: action.token,
+        email: action.email,
+        username: action.username,
+      };
+      break;
+    case 'LOGIN_USER_SUCCESS':
+      localStorage.setItem("token", action.data.token);
+      return userInfo(action);
+    case 'REGISTER_USER_SUCCESS':
+      return userInfo(action);
+    case 'FORGOT_PASSWORD_SUCCESS':
+      return userInfo(action);
+    case 'LOGOUT_SUCCESS':
+      localStorage.removeItem("token");
+      return {
+        token: '',
+        email: '',
+        username: '',
+        user: '',
+        isAuthenticated: false,
+        status: action.status_code
+      }
+    default:
+        return state
+  }
 }
+
 
 export default authReducer;
