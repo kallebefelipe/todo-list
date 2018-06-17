b485b3413b7d4a8cc53e7489f0cfc27142a7e411
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-17 06:22:48 -0300

    Atualizando componente TodoApp

--- assets/js/components/TodoApp.js
+++ assets/js/components/TodoApp.js
@@ -1,31 +1,28 @@
- import React from 'react';
-import PropTypes from 'prop-types';
 import Header from './Header';
-import AddTodoForm from './todos/AddTodoForm';
+import PropTypes from 'prop-types';
+import React from 'react';
 import TodoList from './todos/TodoList';
 import { connect } from 'react-redux';
 import { populateInitialState } from '../actions/todos';
 
 
 class TodoApp extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            data: undefined,
-        };
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      data: undefined,
+      };
     }
 
-    render() {
-        return (
-            <div>
-                <Header />
-                <AddTodoForm />
-                <TodoList />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <TodoList />
+      </div>
+    );
+  }
 }
-
 
 
 export default TodoApp;
