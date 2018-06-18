29f014a835613684e6d2930bbf5ee215677741d6
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-18 15:46:22 -0300

    Adicionando testes de reducer user

---
+++ assets/js/reducers/tests/user.test.js
@@ -0,0 +1,27 @@
+import userReducer from '../user'
+
+
+describe('auth reducer', () => {
+  it('should return the initial state', () => {
+    expect(userReducer(undefined, {})).toEqual(
+      {
+        users: []
+      }
+    )
+  })
+
+  it('should handle LOAD_USERS_SUCCESS', () => {
+    expect(
+      userReducer({}, {
+        type: 'LOAD_USERS_SUCCESS',
+        users: [],
+      })
+    ).toEqual(
+      {
+        users: [],
+      }
+    )
+  })
+})
+
+
