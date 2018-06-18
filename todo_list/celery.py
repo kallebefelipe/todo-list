35b3993e07eff0fa5920f0c5a5f6e48983836d8c
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-18 17:24:13 -0300

    Configurando beat_schedule

--- todo_list/celery.py
+++ todo_list/celery.py
@@ -11,3 +11,11 @@ app = Celery('todo_list_tasks')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 app.conf.broker_url = 'redis://localhost:6379/0'
+
+app.conf.beat_schedule = {
+    'send-email-every-day': {
+        'task': 'todos.tasks.report_task',
+        'schedule': 86400.0
+    },
+}
+app.conf.timezone = 'UTC'
