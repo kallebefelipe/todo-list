829c4752e9d2576102aaf36a0ee35d1d541f5782
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-19 21:23:45 -0300

    Ajustes no estilo do componente header

--- assets/js/components/Header.js
+++ assets/js/components/Header.js
@@ -2,14 +2,14 @@ import AddTodoForm from './todos/AddTodoForm';
 import React from 'react';
 import { connect } from 'react-redux';
 import { logoutUser } from '../actions/auth';
+import { Button } from 'react-bootstrap';
 
 
 const Header = (props) => (
   <header>
     <h1>TODO-LIST APP</h1>
-    <p>Todo-List</p>
       <AddTodoForm />
-    <button onClick={() => {props.mapLogoutUser()}}>Logout</button>
+    <Button onClick={() => {props.mapLogoutUser()}}>Logout</Button>
   </header>
 );
 
