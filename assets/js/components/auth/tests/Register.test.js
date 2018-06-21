ed8493809c87ce1103b915e1dab98126f95227ed
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-21 12:59:53 -0300

    Adicionando teste de componente de registro

---
+++ assets/js/components/auth/tests/Register.test.js
@@ -0,0 +1,39 @@
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { mount } from 'enzyme';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import setUpStore from '../../../store/configureStore';
+import Register from '../Register';
+import { Provider } from 'react-redux';
+
+
+Enzyme.configure({ adapter: new Adapter() });
+const store = setUpStore();
+
+
+function setup() {
+  const props = {
+    token: 'Token',
+  }
+
+  const enzymeWrapper = mount(
+      <Provider store={store} >
+        <Register {...props} />
+      </Provider>
+    )
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+describe('Register', () => {
+  it('should render register', () => {
+    const { enzymeWrapper } = setup()
+
+    const component = renderer.create(<enzymeWrapper />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+})
