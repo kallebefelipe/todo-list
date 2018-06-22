5787c54a231d2eb5208708090ca9e6ab8df4ec59
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-22 12:07:45 -0300

    Atualização de componente de adicionar task

--- assets/js/components/tasks/tests/AddTaskForm.test.js
+++ assets/js/components/tasks/tests/AddTaskForm.test.js
@@ -1,43 +1,23 @@
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { mount } from 'enzyme';
-import fetchMock from 'fetch-mock';
+import AddTaskForm from '../AddTaskForm';
 import React from 'react';
 import renderer from 'react-test-renderer';
-import setUpStore from '../../../store/configureStore';
-import AddTaskForm from '../AddTaskForm';
-import { Provider } from 'react-redux';
 
-
-Enzyme.configure({ adapter: new Adapter() });
-const store = setUpStore();
-
-
-function setup() {
-  const props = {
-    token: 'Token',
-  }
-  const header = {'content-type': 'application/json' }
-
-  fetchMock
-      .getOnce('api/user/', {body: [{name: 'user 1', id: 1}], headers: header})
-
-  const enzymeWrapper = mount(
-      <Provider store={store} >
-        <AddTaskForm {...props} />
-      </Provider>
-    )
-
-  return {
-    props,
-    enzymeWrapper
-  }
-}
 
 describe('AddTaskForm', () => {
-  it('should render todo form add task', () => {
-    const { enzymeWrapper } = setup()
+  it('should render add task form', () => {
 
-    const component = renderer.create(<enzymeWrapper />);
+    const component = renderer.create(
+      <AddTaskForm
+        updateValue={() => {}}
+        name={''}
+        erroName={false}
+        updateDataValue={() => {}}
+        deadline={''}
+        erroDate={false}
+        assignTitle={''}
+        generateUserList={() => {}}
+        handleSubmit={() => {}}
+      />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   })
