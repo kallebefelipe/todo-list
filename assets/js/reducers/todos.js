208c2620b32105e5a8df83fab95ba9de3247344f
Kallebe Felipe <kallebefelipe@gmail.com>
2018-06-22 11:55:40 -0300

    Atualização de reducer de todo para usar action types

--- assets/js/reducers/todos.js
+++ assets/js/reducers/todos.js
@@ -1,4 +1,5 @@
 import { browserHistory } from 'react-router';
+import * as types from '../actionTypes';
 
 
 const todosState = {
@@ -8,26 +9,26 @@ const todosState = {
 
 const todoReducer = (state= todosState, action) => {
   switch(action.type) {
-    case 'POPULATE_INITIAL_STATE':
+    case types.POPULATE_INITIAL_STATE:
       return {
         todos: action.data
       };
-    case 'ADD_TODO_SUCCESS':
+    case types.ADD_TODO_SUCCESS:
       return {
         todos: state.todos.concat(action.todo)
       };
-    case 'LOAD_TODO_SUCCESS':
+    case types.LOAD_TODO_SUCCESS:
      return {
         todos: action.todos
       };
-    case 'DELETE_TODO_SUCCESS': {
+    case types.DELETE_TODO_SUCCESS: {
       return {
         todos: state.todos.filter((todo) => {
           return todo.id !== action.todo.id
         })
       };
     }
-    case 'UPDATE_TODO_SUCCESS': {
+    case types.UPDATE_TODO_SUCCESS: {
       return {
         todos: state.todos.map((todo) => {
           if (todo.id === action.todo.id){
@@ -37,7 +38,7 @@ const todoReducer = (state= todosState, action) => {
         })
       };
     }
-    case 'ADD_TASK_SUCCESS': {
+    case types.ADD_TASK_SUCCESS: {
       const todos = state.todos.map((todo) => {
         if (todo.id === action.data.todo){
             todo.tasks = todo.tasks.concat(action.data);
@@ -49,7 +50,7 @@ const todoReducer = (state= todosState, action) => {
           todos
       };
     }
-    case 'UPDATE_TASK_SUCCESS': {
+    case types.UPDATE_TASK_SUCCESS: {
       const todos = state.todos.map((todo) => {
         if (todo.id === action.task.todo){
           todo.tasks = todo.tasks.map((task) => {
@@ -66,7 +67,7 @@ const todoReducer = (state= todosState, action) => {
           todos
       };
     }
-    case 'DELETE_TASK_SUCCESS': {
+    case types.DELETE_TASK_SUCCESS: {
       const todos = state.todos.map((todo) => {
         if (todo.id === action.todo_id){
           todo.tasks = todo.tasks.filter((task) => {
